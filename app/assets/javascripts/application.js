3c877481775ca317781e1159ccd47709b31e83f6
benedicttt <benedicttt27051987@gmail.com>
2022-11-03 01:45:53 +0200

    fixed style for mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
                 position: 'static',
                 bottom: 'auto',
                 left: 'auto',
-                zoom: 0.5
+                zoom: 0.6
             });
         }
         if ($containerHeight > 819) {
