11f4903061469db0ffeb947534eae218efa5f900
benedicttt <benedicttt27051987@gmail.com>
2022-11-06 22:43:18 +0200

    redirect to worklist last

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,18 +8,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user_email = User.find_by(email: params[:session][:email], password: params[:session][:password])
-    user_nick = User.find_by(nickname: params[:session][:nickname], password: params[:session][:password])
+    user_email = User.find_by(email: params[:email], password: params[:password])
+    user_nick = User.find_by(nickname: params[:nickname], password: params[:password])
 
-    session = if !params[:session][:nickname].blank? && !user_nick.nil?
+    session = if !params[:nickname].blank? && !user_nick.nil?
                 log_in user_nick
-              elsif !params[:session][:email].blank? && !user_email.nil?
+              elsif !params[:email].blank? && !user_email.nil?
                 log_in user_email
               end
 
     if session
       logger.info session
-      redirect_to root_path
+      # redirect_to root_path
+
+      wl = WorkList.where(user_id: current_user.id).last
+      params.merge!(
+        user_id: current_user.id,
+        month: wl.month,
+        year: wl.years,
+      )
+
+      params.merge!(head: "New work list for user:  #{current_user.first_name} #{current_user.last_name},  month: #{params[:month]},  year: #{params[:year]}")
+
+      render "work_lists/work_list_new", params: params
 
     else
       flash[:success] = ""
